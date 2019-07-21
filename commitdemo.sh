306b8d8207221bb750ae7ecb76161f725ab26db9
RoomTu <2446684021@qq.com>
2019-07-21 23:19:34 +0800

    第一次更新

--- commitdemo.sh
+++ commitdemo.sh
@@ -1,11 +1,17 @@
 if [ -d $1 ]; then
-  echo 'error: dir exists'
+  # echo 'error: dir exists'
   exit 1
 else
   mkdir $1
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
+  echo '<!DOCTYPE>' > index.html
+  echo '<title>hello</title>' >> index.html
+  echo '<h1>hi</h1>' >>index.html
+  echo 'h1{color: red;}' > css/style.css
+  echo "var string = 'hello world'" >js/main.js
+  echo 'alert(string)' >> js/main.js
   echo 'success'
   exit 0
 fi
--- jsdemo.js
+++ jsdemo.js
@@ -9,8 +9,14 @@ process.chdir("./" + dirName) // cd $1
 fs.mkdirSync('css') // mkdir css
 fs.mkdirSync('js') // mkdir js
 
-fs.writeFileSync("./index.html", "")
-fs.writeFileSync("css/style.css", "")
-fs.writeFileSync("./js/main.js", "")
+//输入html，css，js内容
+var htmlContent,cssContent,jsContent;
+htmlContent = "<!DOCTYPE><title>hello</title><h1>hi</h1>";
+cssContent = "h1{color: red;}";
+jsContent = "var string = 'hello world';alert(string);"
+
+fs.writeFileSync("./index.html", htmlContent)
+fs.writeFileSync("css/style.css", cssContent)
+fs.writeFileSync("./js/main.js", jsContent)
 
 process.exit(0)
